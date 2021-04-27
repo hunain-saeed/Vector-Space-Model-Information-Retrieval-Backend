5cc9461fc4e6723c315ac800ab272d44103b4011
hunain631 <hunainsaeed631@gmail.com>
2021-04-27 06:10:26 +0500

    Backend initialization done

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+# from flask_cors import CORS
+
+
+app = Flask(__name__)
+# CORS(app)
+
+
+@app.route('/')
+def index():
+    return "Hello, World"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
