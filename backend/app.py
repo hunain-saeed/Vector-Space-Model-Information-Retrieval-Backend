70fc96ad0532443e61f18bcbb3ee557112c52390
hunain631 <hunainsaeed631@gmail.com>
2021-04-27 06:16:45 +0500

    initialization done

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,17 @@
+import vsm
 from flask import Flask
-# from flask_cors import CORS
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
 
 @app.route('/')
 def index():
     return "Hello, World"
 
+app.add_url_rule('/h', view_func=vsm.hello_world)
 
 
 if __name__ == "__main__":
---
+++ backend/vsm.py
@@ -0,0 +1,4 @@
+import json
+
+def hello_world():
+    return json.dumps({"hello": "hello world"})
